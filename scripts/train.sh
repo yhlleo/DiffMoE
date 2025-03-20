0bbfd8180c4e18b5a45d3c1eb8b42ea072804c60
shiml20 <1454853976@qq.com>
2025-03-20 11:56:55 +0800

    [upd] README

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,10 +5,3 @@ CUDA_VISIBLE_DEVICES=0 torchrun \
     --master_port=12345 \
     train.py \
     --config config/000_DiffMoE_S_E16_Flow.yaml
-
-# example cache
-torchrun --nproc_per_node=8 --nnodes=1 --node_rank=0 --master_port=12345 \
-    main_cache.py \
-    --img_size 128 --vae_path stabilityai/sd-vae-ft-mse --vae_embed_dim 16 \
-    --batch_size 256 \
-    --data_path ${IMAGENET_PATH} --cached_path cache/
